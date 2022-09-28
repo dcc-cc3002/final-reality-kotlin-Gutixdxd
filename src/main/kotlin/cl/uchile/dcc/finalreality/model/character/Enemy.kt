bb34f0b7c7147d0de013e847b81594111c768400
Gutixdxd <i.gutierrez@ing.uchile.cl>
2022-09-28 18:52:25 -0300

    (Style)Enemy Equals style changed to follow Ktlinter guidelines

--- src/main/kotlin/cl/uchile/dcc/finalreality/model/character/Enemy.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/character/Enemy.kt
@@ -1,7 +1,7 @@
 package cl.uchile.dcc.finalreality.model.character
 
 import cl.uchile.dcc.finalreality.exceptions.Require
-import java.util.*
+import java.util.Objects
 import java.util.concurrent.BlockingQueue
 
 /**
@@ -31,16 +31,16 @@ class Enemy(
     val weight = Require.Stat(weight, "Weight") atLeast 1
     val attack = Require.Stat(attack, "Attack") atLeast 0
     override fun equals(other: Any?) = when {
-        this === other                 -> true
-        other !is Enemy                -> false
+        this === other -> true
+        other !is Enemy -> false
         hashCode() != other.hashCode() -> false
-        name != other.name             -> false
-        weight != other.weight         -> false
-        maxHp != other.maxHp           -> false
-        defense != other.defense       -> false
-        currentHp != other.currentHp   -> false
-        attack != other.attack         -> false
-        else                           -> true
+        name != other.name -> false
+        weight != other.weight -> false
+        maxHp != other.maxHp -> false
+        defense != other.defense -> false
+        currentHp != other.currentHp -> false
+        attack != other.attack -> false
+        else -> true
     }
 
     override fun hashCode() = Objects.hash(Enemy::class, name, weight, maxHp, defense)
